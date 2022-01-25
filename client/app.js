0e4b0d961aa259a98cd3e2503fbaa2aa0328382d
冯箭飞 <fengjf@gistack.cn>
2022-01-25 17:15:05 +0800

    爬取多页及链接错处处理

--- client/app.js
+++ client/app.js
@@ -1,5 +1,8 @@
 import Spider from './spider.js'
 import path from 'node:path';
 
-const spider = new Spider('https://wallhaven.cc/random?seed=n3Jyc7&page=1');
+const spider = new Spider({
+    base_url: 'https://wallhaven.cc/random?seed=n3Jyc7&page=',
+    maxPage: 10,
+});
 spider.init();
--- client/spider.js
+++ client/spider.js
@@ -5,18 +5,33 @@ import fs from 'node:fs';
 
 class Spider {
 
-    constructor(url) {
-        this.base_url = url; //要爬取的网站
+    constructor(props) {
+        this.base_url = props.base_url; // 要爬取的网站
         this.current_page = 1;
         this.result_list = [];
+        this.maxPage = props.maxPage; // 最大页数
     }
 
     async init() {
         try {
-            await this.getPageData();
+            await this.getPicLinks();
             await this.downLoadPictures();
         } catch (e) {
             console.log(e);
+        }
+    }
+
+    async getPicLinks() {
+        try {
+            for(let page = 1; page <= this.maxPage; page++){
+                console.log(`开始抓取第${page}页图片链接`)
+                await this.getPageData(this.base_url + page);
+                console.log(`第${page}页图片链接抓取完成`)
+                await this.sleep(3000 * Math.random());
+            }
+            return Promise.resolve();
+        } catch (e) {
+            Promise.reject(e);
         }
     }
 
@@ -29,8 +44,8 @@ class Spider {
         });
     }
 
-    async getPageData() {
-        const target_url = this.base_url;
+    async getPageData(href) {
+        const target_url = href;
         try {
             const res = await axios.get(target_url);
             const html = res.data;
@@ -42,9 +57,7 @@ class Spider {
                 datasrcs[datasrcs.length - 1] = 'wallhaven-' + datasrcs[datasrcs.length - 1];
                 let src = datasrcs.join('/');
                 src = src.replace(/th\.wallhaven\.cc\/small/, 'w.wallhaven.cc/full')
-                result_list.push({
-                    down_loda_url: src,
-                });
+                result_list.push(src);
             });
             this.result_list.push(...result_list);
             return Promise.resolve(result_list);
@@ -59,9 +72,9 @@ class Spider {
         try {
             for (let i = 0, len = result_list.length; i < len; i++) {
                 console.log(`开始下载第${i + 1}张图片!`);
-                await this.downLoadPicture(result_list[i].down_loda_url);
-                await this.sleep(3000 * Math.random());
+                await this.downLoadPicture(result_list[i]);
                 console.log(`第${i + 1}张图片下载成功!`);
+                await this.sleep(3000 * Math.random());
             }
             return Promise.resolve();
         } catch (e) {
@@ -77,6 +90,21 @@ class Spider {
             });
             await response.data.pipe(fs.createWriteStream(target_path));
             console.log('写入成功');
+            return Promise.resolve();
+        } catch (e) {
+            const reg = /\.png/
+            if(reg.test(href)){
+                return Promise.reject(e)
+            }else{
+                await this.jpg2png(href);
+            }
+        }
+    }
+
+    async jpg2png(href) {
+        try {
+            const href4png = href.replace(/\.jpg/, '.png')
+            await this.downLoadPicture(href4png)
             return Promise.resolve();
         } catch (e) {
             return Promise.reject(e)
